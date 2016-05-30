59854ef15575420f3b7299f245f801f560743dea
Lyall Jonathan Di Trapani <jonathan.ditrapani@manheim.com>
2016-05-30 12:20:27 -0400

    Trying out FastParse

--- build.sbt
+++ build.sbt
@@ -1,8 +1,11 @@
-lazy val root = (project in file(".")).
-  settings(
-    name := "Tile Creator",
-    version := "0.1",
-    scalaVersion := "2.11.8"
-  )
+lazy val root = (project in file(".")).settings(
+  name := "tile-creator",
+  version := "0.1.0",
+  scalaVersion := "2.11.8",
+  organization := "ditrapani.info"
+)
 
+libraryDependencies += "com.lihaoyi" %% "fastparse" % "0.3.7"
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+
+assemblyJarName in assembly := s"ljd-${name.value}-${version.value}.jar"
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,26 @@
 package info.ditrapani.tilecreator
 
+import scala.util.{Try, Success, Failure}
+
 object Main {
-  def main(args: Array[String]): Unit = println("Hello world!")
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+    val file_name = "src/test/resources/built-in.tiles"
+
+    Try(scala.io.Source.fromFile(file_name).mkString) match {
+      case Failure(exception) => println(exception)
+      case Success(str) => parseStr(str)
+    }
+  }
+
+  def parseStr(str: String): Unit = {
+    import fastparse.all._
+
+    val parseFile = P("")
+    val parseA = P("a")
+    println(parseA.parse("a"))
+    println(parseA.parse("b"))
+  }
 }
 
 case class Adder(val base: Int) {
