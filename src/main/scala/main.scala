1b87fe5b184e10c6a840f403e7381422d1cda723
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-05-30 14:58:31 -0400

    Get parsing working; thanks Haoyi!

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,10 +1,10 @@
 package info.ditrapani.tilecreator
 
-import scala.util.{Try, Success, Failure}
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    import scala.util.{Try, Success, Failure}
+
     val file_name = "src/test/resources/built-in.tiles"
 
     Try(scala.io.Source.fromFile(file_name).mkString) match {
@@ -16,10 +16,51 @@ object Main {
   def parseStr(str: String): Unit = {
     import fastparse.all._
 
-    val parseFile = P("")
-    val parseA = P("a")
-    println(parseA.parse("a"))
-    println(parseA.parse("b"))
+    val large_tile_header = P("Large Tiles\n")
+    val large_tile_ruler = P("  0 1 2 3 4 5 6 7 8 9 A B C D E F\n")
+    val pixel0 = P("[]").map((x) => 0)
+    val pixel1 = P("()").map((x) => 1)
+    val pixel2 = P("{}").map((x) => 2)
+    val pixel3 = P("<>").map((x) => 3)
+    val pixel = P(pixel0 | pixel1 | pixel2 | pixel3)
+    val large_tile_row = P("  " ~/ pixel.rep(min = 16, max = 16))
+    val tile_num = P(CharIn("0123456789ABCDEF").rep(min = 2, max = 2) ~/ "\n")
+    val large_tile =
+      P(tile_num ~/ large_tile_row.rep(min = 16, max = 16, sep = "\n") ~/ "\n")
+    val large_tile_set = P(
+      large_tile_header ~/
+      large_tile_ruler ~/
+      large_tile.rep(min = 64, max = 64)
+    )
+    val small_tile_header = P("Small Tiles\n")
+    val small_tile_ruler = P("  0 1 2 3 4 5 6 7\n")
+    val small_tile_row = P("  " ~/ pixel.rep(min = 8, max = 8))
+    val small_tile =
+      P(tile_num ~/ small_tile_row.rep(min = 8, max = 8, sep = "\n") ~/ "\n")
+    val small_tile_set = P(
+      small_tile_header ~/
+      small_tile_ruler  ~/
+      small_tile.rep(min = 64, max = 64)
+    )
+    val text_tile_header = P("Text Char Tiles\n")
+    val text_pixel = P(pixel0 | pixel3)
+    val text_tile_row = P("  " ~/ text_pixel.rep(min = 8, max = 8))
+    val text_tile =
+      P(tile_num ~/ text_tile_row.rep(min = 8, max = 8, sep = "\n") ~/ "\n")
+    val text_tile_set = P(
+      text_tile_header ~/
+      small_tile_ruler  ~/
+      text_tile.rep(min = 128, max = 128)
+    )
+    val parseFile = P(
+      Start ~/
+      large_tile_set ~/ "\n" ~/ small_tile_set ~/ "\n" ~/ text_tile_set ~/
+      End
+    )
+    parseFile.parse(str) match {
+      case Parsed.Success(value, index) => println(s"Success ${value} ${index}")
+      case x: Parsed.Failure => println(s"${x}")
+    }
   }
 }
 
