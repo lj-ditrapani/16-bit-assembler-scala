ac980da867a26c1a1daa92b5df4f3f33f9845702
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-06-05 13:09:04 -0400

    WIP: trying to get basics of asm parsing down

--- src/main/scala/assembler.scala
+++ src/main/scala/assembler.scala
@@ -2,6 +2,43 @@ package info.ditrapani.asm
 
 object Assembler {
   def apply(text: String): Either[String, Seq[Byte]] = {
+    import fastparse.all._
+
+    val valid_cahrs = P(
+      Start ~/ ("\n" | CharIn('\u0020' to '\u007E')).rep ~/ End
+    )
+    println(valid_cahrs.parse(text))
+
+    val comment = P("#" ~/ CharsWhile(_ != '\n', min = 0))
+    val spaces = P(" ".rep(1))
+
+    val tail_noise = P((spaces ~/ comment) | " ".rep)
+
+    val noise = P(" ".rep ~ comment.? ~ "\n").rep
+
+    val digit = P(CharIn('0' to '9'))
+    val uppercase = P(CharIn('A' to 'Z'))
+    val lowercase = P(CharIn('a' to 'z'))
+    val letter = P(lowercase | uppercase)
+    val number = P(digit.rep(1).!.map(_.toInt))
+    val symbol = P(letter ~/ (letter | digit | "-" | "_").rep(min = 0)).!
+    val symbol_entry = P(
+      noise ~/ " ".rep ~/ symbol ~/ spaces ~/ number ~/ tail_noise
+    )
+    val symbols_section = P(
+      ".symbols\n" ~/ Index ~/ symbol_entry.rep ~/ noise ~/ ".end-symbols"
+    )
+    val file = P(Start ~/ noise ~/ symbols_section)
+    // Start ~/ noise ~/ symbols_section ~/
+    //   noise ~/ data_section ~/
+    //   noise ~/ program_section ~/
+    //   noise ~/ End
+    //   command = command_stuff ~/ P(tail_noise ~ "\n")
+    val s = file.parse(text) match {
+      case f: Parsed.Failure => f.toString + "\n" + f.extra.traced.trace.split("/").mkString("\n")
+      case s: Parsed.Success[Int] => s.toString
+    }
+    println(s)
     Right(Vector(65, 66, 67).map(_.toByte))
   }
 }
--- todo.md
+++ todo.md
@@ -1,3 +1,7 @@
 - main assembler parser
+    - dissallow all control chars except '\n'
+    - optional .?, negation !()
+    - Can flat_map over classes that inject state and return new parsers
+      to allow immutable symbol map & line number propagation.
 - scene parser (uses bg-cell parser)
 - bg-cell parser
