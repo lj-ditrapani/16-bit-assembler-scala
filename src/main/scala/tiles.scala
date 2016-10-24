d345ea4586b517e7e980fb3c05ddb2591b055378
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-10-24 11:19:39 -0400

    Rewrite tile parser for new video system
    Now that the video system only has one layer of 1 bpp tiles,
    the ASCII .tiles file format and binary tile file are very different.

--- src/main/scala/tiles.scala
+++ src/main/scala/tiles.scala
@@ -4,51 +4,22 @@ object Tiles {
   def parseStr(str: String): Either[String, Seq[Byte]] = {
     import fastparse.all._
 
-    val tile_ruler = P("  0 1 2 3 4 5 6 7 8 9 A B C D E F\n")
+    val tile_ruler = P("  0 1 2 3 4 5 6 7\n")
     val pixel0 = P("[]").map((x) => 0)
-    val pixel1 = P("()").map((x) => 1)
-    val pixel2 = P("{}").map((x) => 2)
-    val pixel3 = P("<>").map((x) => 3)
-    val pixel = P(pixel0 | pixel1 | pixel2 | pixel3)
-    val large_tile_row = P("  " ~/ pixel.rep(min = 16, max = 16)).map(Large.row)
+    val pixel1 = P("<>").map((x) => 1)
+    val pixel = P(pixel0 | pixel1)
+    val tile_row = P("  " ~/ pixel.rep(min = 8, max = 8)).map(Tile.row)
     val tile_num = P(CharIn("0123456789ABCDEF").rep(min = 2, max = 2) ~/ "\n")
-    val large_tile = P(
-      tile_num ~/ large_tile_row.rep(min = 16, max = 16, sep = "\n") ~/ "\n"
-    ).map(_.flatten)
-    val large_tile_set = P(
-      large_tile_header ~/
-      large_tile_ruler ~/
-      large_tile.rep(min = 64, max = 64)
-    ).map(_.flatten)
-    val small_tile_header = P("Small Tiles\n")
-    val small_tile_ruler = P("  0 1 2 3 4 5 6 7\n")
-    val small_tile_row = P("  " ~/ pixel.rep(min = 8, max = 8)).map(Small.row)
-    val small_tile = P(
-      tile_num ~/ small_tile_row.rep(min = 8, max = 8, sep = "\n") ~/ "\n"
-    ).map(_.flatten)
-    val small_tile_set = P(
-      small_tile_header ~/
-      small_tile_ruler  ~/
-      small_tile.rep(min = 64, max = 64)
-    ).map(_.flatten)
-    val text_tile_header = P("Text Char Tiles\n")
-    val text_pixel0 = P("[]").map((x) => 0)
-    val text_pixel1 = P("<>").map((x) => 1)
-    val text_pixel = P(text_pixel0 | text_pixel1)
-    val text_tile_row =
-      P("  " ~/ text_pixel.rep(min = 8, max = 8)).map(Text.row)
-    val text_tile =
-      P(tile_num ~/ text_tile_row.rep(min = 8, max = 8, sep = "\n") ~/ "\n")
-    val text_tile_set = P(
-      text_tile_header ~/
-      small_tile_ruler  ~/
-      text_tile.rep(min = 128, max = 128)
+    val tile = P(
+      tile_num ~/ tile_row.rep(min = 12, max = 12, sep = "\n") ~/ "\n"
+    )
+    val tile_set = P(
+      tile_ruler ~/
+      tile.rep(min = 256, max = 256)
     ).map(_.flatten)
     val parseFile = P(
-      Start ~/
-      large_tile_set ~/ "\n" ~/ small_tile_set ~/ "\n" ~/ text_tile_set ~/
-      End
-    ).map((x) => x._1 ++ x._2 ++ x._3)
+      Start ~/ tile_set ~/ End
+    )
     parseFile.parse(str) match {
       case Parsed.Success(value, index) => Right(value)
       case x: Parsed.Failure => Left(s"${x}")
@@ -68,26 +39,9 @@ object Utils {
   }
 }
 
-object Large {
-  type Row = Seq[Byte]
-
-  def row(seq: Seq[Int]): Row = {
-    val (seq1, seq2) = seq.splitAt(8)
-    Utils.eightPixels2twoBytes(seq1) ++ Utils.eightPixels2twoBytes(seq2)
-  }
-}
-
-object Small {
-  type Row = Seq[Byte]
-
-  def row(seq: Seq[Int]): Row = {
-    Utils.eightPixels2twoBytes(seq)
-  }
-}
-
-object Text {
-  type Tile = Vector[Byte]
+object Tile {
   def row(seq: Seq[Int]): Byte = {
+    assert(seq.length == 8)
     val unsummed_vals: Seq[Int] =
       for ((v: Int, i: Int) <- seq.zipWithIndex) yield {
         v << (7 - i)
