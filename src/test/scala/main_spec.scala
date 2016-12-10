399ebfd1306426d6ee7e873e9f2c747cdac2d35f
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-12-10 06:28:21 -0500

    Refactor Main.process specs

--- src/test/scala/main_spec.scala
+++ src/test/scala/main_spec.scala
@@ -4,17 +4,25 @@ import org.scalatest.EitherValues
 
 class MainSpec extends Spec with EitherValues {
   describe("process") {
-    def expectError(args: Array[String], message: String): Unit = {
+    def expectError(result: Result, message: String): Unit = {
+      result shouldBe an[Error]
+      @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
+      val value = result.asInstanceOf[Error].message
+      value shouldBe message
+    }
+
+    @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
+    def expectGood(result: Result): Seq[Byte] = {
+      result shouldBe a[Good]
+      result.asInstanceOf[Good].bytes
     }
 
     describe("with 0 args") {
       it("prints an error") {
         val args = Array[String]()
         val result = Main.process(args)
-        result shouldBe an[Error]
-        @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
-        val value = result.asInstanceOf[Error].message
-        value shouldBe "Missing command line arguments; requires 1 or 2"
+        val message = "Missing command line arguments; requires 1 or 2"
+        expectError(result, message)
       }
     }
 
@@ -31,29 +39,33 @@ class MainSpec extends Spec with EitherValues {
         result shouldBe Help
       }
 
-      describe("arg is neither -h nor --help") {
-        it("raises no such file exception") {
+      describe("arg is a file") {
+        it("assembles the file into a binary")(pending)
+      }
+
+      describe("arg is neither a file nor -h nor --help") {
+        it("prints an error") {
           val args = Array("--one")
           val result = Main.process(args)
-          result shouldBe an[Error]
-          @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
-          val value = result.asInstanceOf[Error].message
-          value shouldBe "java.io.FileNotFoundException: --one " +
+          val message = "java.io.FileNotFoundException: --one " +
             "(No such file or directory)"
+          expectError(result, message)
         }
       }
     }
 
     describe("with 2 args") {
+      def checkTiles(result: Result): Unit = {
+        val value = expectGood(result)
+        value.length shouldBe 3072
+        value(12 + 1) & 0xFF shouldBe 0x66   //  []<><>[][]<><>[]  0110 0110
+      }
+
       describe("first arg = -t") {
         it("transforms a good ascii tile file into a binary one") {
           val args = Array("-t", "src/test/resources/built-in.tiles")
           val result = Main.process(args)
-          result shouldBe a[Good]
-          @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
-          val value = result.asInstanceOf[Good].bytes
-          value.length shouldBe 3072
-          value(12 + 1) & 0xFF shouldBe 0x66   //  []<><>[][]<><>[]  0110 0110
+          checkTiles(result)
         }
       }
 
@@ -61,11 +73,7 @@ class MainSpec extends Spec with EitherValues {
         it("transforms a good ascii tile file into a binary one") {
           val args = Array("-t", "src/test/resources/built-in.tiles")
           val result = Main.process(args)
-          result shouldBe a[Good]
-          @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
-          val value = result.asInstanceOf[Good].bytes
-          value.length shouldBe 3072
-          value(12 + 1) & 0xFF shouldBe 0x66   //  []<><>[][]<><>[]  0110 0110
+          checkTiles(result)
         }
       }
 
@@ -73,11 +81,9 @@ class MainSpec extends Spec with EitherValues {
         it("first arg is neither -t nor --tiles") {
           val args = Array("--one", "file")
           val result = Main.process(args)
-          result shouldBe an[Error]
-          @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
-          val value = result.asInstanceOf[Error].message
-          value shouldBe "The two argument form is to create binary tile sets, " +
+          val message = "The two argument form is to create binary tile sets, " +
             "the first argument must be -t or --tiles."
+          expectError(result, message)
         }
       }
     }
@@ -86,10 +92,8 @@ class MainSpec extends Spec with EitherValues {
       it("prints an error") {
         val args = Array("--one", "--two", "--three")
         val result = Main.process(args)
-        result shouldBe an[Error]
-        @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
-        val value = result.asInstanceOf[Error].message
-        value shouldBe "Too many command line arguments; supply 1 or 2"
+        val message = "Too many command line arguments; supply 1 or 2"
+        expectError(result, message)
       }
     }
   }
